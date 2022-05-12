6edcb014f9ec3d958c72f1dce327af3dd3cdd413
h-uwimana <302211792@student.rocmondriaan.nl>
2022-05-12 15:23:34 +0200

    a

---
+++ src/data/countyData.js
@@ -0,0 +1,29 @@
+const countryData = {
+    countries: [
+        {
+            id:1,
+            name: 'USA',
+            capital: 'Washington',
+            cost: 1250,
+            details: 'United States are among the most visited',
+            img: 'washington.jpg'
+        },
+        {
+            id:2,
+            name: 'Netherlands',
+            capital: 'Amsterdam',
+            cost: 795,
+            details: 'The capital of the Netherlands',
+            img: 'amsterdam.jpg'
+        },
+        {
+            id:3,
+            name: 'Germany',
+            capital: 'Berlin',
+            cost: 895,
+            details: 'The capital of Germany',
+            img: 'berlin.jpg'
+        },
+    ]
+}
+export default countryData;
